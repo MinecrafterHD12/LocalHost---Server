e741701e6d2dfe0314ebb3e2b47526851464d6bc
MinecrafterHD12 <41869064+MinecrafterHD12@users.noreply.github.com>
2018-08-31 14:39:00 +0200

    Alle Datein Hochgeladen

---
+++ start.bat
@@ -0,0 +1,13 @@
+title Spigot - Server - v1.8
+
+@echo Data import By MinecrafterHD12
+
+@echo Alle Daten sind geladen
+
+@echo off
+
+"C:\Program Files\Java\jre1.8.0_181\bin\java.exe" -Xms532M -Xmx1032M -jar spigot.jar
+
+@echo Alle Daten sind Aktiv
+
+pause
